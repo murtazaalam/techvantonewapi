796e315727a7fb0d430c1bac8605205396ed6721
Murtuza <34296103+murtazaalam@users.noreply.github.com>
2022-02-16 11:49:09 +0530

    event collection changed

--- models/event.js
+++ models/event.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const events = new Schema({});
-module.exports = mongoose.model("events",events);
+const techvanto_events = new Schema({});
+module.exports = mongoose.model("techvanto_events",techvanto_events);
