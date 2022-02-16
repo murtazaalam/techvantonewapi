d6ce5cb125a9d1648f519bf2c19101240a61b12e
Murtuza <34296103+murtazaalam@users.noreply.github.com>
2022-02-16 11:28:50 +0530

    corss error resolving

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(cors());   
+app.use(cors());  
+ 
 
 // app.all('/*', function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
